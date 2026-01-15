3176b61b5b02cb751c1360517731cf7874657c5c
Roger Paniura Ramos <192218@unamba.edu.pe>
2026-01-14 19:17:26 -0500

    initial commit

---
+++ Adoptme/src/app.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cookieParser from 'cookie-parser';
+
+import usersRouter from './routes/users.router.js';
+import petsRouter from './routes/pets.router.js';
+import adoptionsRouter from './routes/adoption.router.js';
+import sessionsRouter from './routes/sessions.router.js';
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose.connect(MONGO_URI)
+  .then(() => console.log("✅ MongoDB conectado"))
+  .catch(err => console.error("❌ MongoDB error:", err));
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use('/api/users', usersRouter);
+app.use('/api/pets', petsRouter);
+app.use('/api/adoptions', adoptionsRouter);
+app.use('/api/sessions', sessionsRouter);
+
+app.listen(PORT, () => console.log(`🚀 Listening on port ${PORT}`));
