96414e904ea83b2f96256b9a8d0480150979b4ba
Roger Paniura Ramos <192218@unamba.edu.pe>
2026-01-14 19:23:06 -0500

    corregir variables de entorno

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
+import 'dotenv/config'; // Carga las variables del archivo .env
 
 import usersRouter from './routes/users.router.js';
 import petsRouter from './routes/pets.router.js';
@@ -8,15 +9,21 @@ import adoptionsRouter from './routes/adoption.router.js';
 import sessionsRouter from './routes/sessions.router.js';
 
 const app = express();
-const PORT = process.env.PORT||8080;
-const connection = mongoose.connect(`mongodb+srv://rogerpaniuramos08_db_user:1234@cluster0.o2asjmp.mongodb.net/?appName=Cluster0`)
+
+// Uso de la variable de entorno para el puerto
+const PORT = process.env.PORT || 8080;
+
+// Uso de la variable de entorno para la conexión a MongoDB
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => console.log("Conectado a MongoDB con éxito"))
+    .catch(err => console.error("Error conectando a MongoDB:", err));
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/api/users',usersRouter);
-app.use('/api/pets',petsRouter);
-app.use('/api/adoptions',adoptionsRouter);
-app.use('/api/sessions',sessionsRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/pets', petsRouter);
+app.use('/api/adoptions', adoptionsRouter);
+app.use('/api/sessions', sessionsRouter);
 
-app.listen(PORT,()=>console.log(`Listening on ${PORT}`))
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
